341f32810557bebebfe20c6a550ebd687b51e095
mesxx <ichsanfauzan2@gmail.com>
2022-12-15 01:37:18 +0700

    Add project files.

---
+++ Calculator_App/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Calculator_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.input1 = new System.Windows.Forms.TextBox();
+            this.input2 = new System.Windows.Forms.TextBox();
+            this.result = new System.Windows.Forms.TextBox();
+            this.sum = new System.Windows.Forms.Button();
+            this.minus = new System.Windows.Forms.Button();
+            this.times = new System.Windows.Forms.Button();
+            this.divided = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // input1
+            // 
+            this.input1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.input1.Location = new System.Drawing.Point(380, 146);
+            this.input1.Name = "input1";
+            this.input1.Size = new System.Drawing.Size(100, 20);
+            this.input1.TabIndex = 0;
+            // 
+            // input2
+            // 
+            this.input2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.input2.Location = new System.Drawing.Point(380, 172);
+            this.input2.Name = "input2";
+            this.input2.Size = new System.Drawing.Size(100, 20);
+            this.input2.TabIndex = 1;
+            // 
+            // result
+            // 
+            this.result.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.result.Location = new System.Drawing.Point(380, 198);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(100, 20);
+            this.result.TabIndex = 2;
+            // 
+            // sum
+            // 
+            this.sum.Location = new System.Drawing.Point(324, 224);
+            this.sum.Name = "sum";
+            this.sum.Size = new System.Drawing.Size(75, 23);
+            this.sum.TabIndex = 3;
+            this.sum.Text = "+";
+            this.sum.UseVisualStyleBackColor = true;
+            this.sum.Click += new System.EventHandler(this.sum_Click);
+            // 
+            // minus
+            // 
+            this.minus.Location = new System.Drawing.Point(405, 224);
+            this.minus.Name = "minus";
+            this.minus.Size = new System.Drawing.Size(75, 23);
+            this.minus.TabIndex = 4;
+            this.minus.Text = "-";
+            this.minus.UseVisualStyleBackColor = true;
+            this.minus.Click += new System.EventHandler(this.minus_Click);
+            // 
+            // times
+            // 
+            this.times.Location = new System.Drawing.Point(324, 253);
+            this.times.Name = "times";
+            this.times.Size = new System.Drawing.Size(75, 23);
+            this.times.TabIndex = 5;
+            this.times.Text = "x";
+            this.times.UseVisualStyleBackColor = true;
+            this.times.Click += new System.EventHandler(this.times_Click);
+            // 
+            // divided
+            // 
+            this.divided.Location = new System.Drawing.Point(405, 253);
+            this.divided.Name = "divided";
+            this.divided.Size = new System.Drawing.Size(75, 23);
+            this.divided.TabIndex = 6;
+            this.divided.Text = "/";
+            this.divided.UseVisualStyleBackColor = true;
+            this.divided.Click += new System.EventHandler(this.divided_Click);
+            // 
+            // clear
+            // 
+            this.clear.Location = new System.Drawing.Point(324, 282);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(156, 23);
+            this.clear.TabIndex = 7;
+            this.clear.Text = "Clear";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(321, 148);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Number 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(321, 174);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Number 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(321, 200);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Result";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.divided);
+            this.Controls.Add(this.times);
+            this.Controls.Add(this.minus);
+            this.Controls.Add(this.sum);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.input2);
+            this.Controls.Add(this.input1);
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox input1;
+        private System.Windows.Forms.TextBox input2;
+        private System.Windows.Forms.TextBox result;
+        private System.Windows.Forms.Button sum;
+        private System.Windows.Forms.Button minus;
+        private System.Windows.Forms.Button times;
+        private System.Windows.Forms.Button divided;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Calculator_App/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void sum_Click(object sender, EventArgs e)
+        {
+            float Sum = float.Parse(input1.Text) + float.Parse(input2.Text);
+            result.Text= Sum.ToString();
+        }
+
+        private void minus_Click(object sender, EventArgs e)
+        {
+            float Min = float.Parse(input1.Text) - float.Parse(input2.Text);
+            result.Text = Min.ToString();
+        }
+
+        private void times_Click(object sender, EventArgs e)
+        {
+            float Times = float.Parse(input1.Text) * float.Parse(input2.Text);
+            result.Text = Times.ToString();
+        }
+
+        private void divided_Click(object sender, EventArgs e)
+        {
+            float Divided = float.Parse(input1.Text) / float.Parse(input2.Text);
+            result.Text = Divided.ToString();
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            input1.Text= string.Empty;
+            input2.Text= string.Empty;
+            result.Text= string.Empty;
+        }
+    }
+}
